13324ca3c4e58e147eb9936983427df3e419b055
mariana10github09 <mariana200710granda@gmail.com>
2025-12-06 00:54:12 -0500

    Change back button background color

--- components/back-button.tsx
+++ components/back-button.tsx
@@ -9,7 +9,7 @@ export default function BackButton() {
   return (
     <button
       onClick={() => router.push("/presents")}
-      className="fixed bottom-6 right-6 z-50 w-12 h-12 bg-[#a13a3a] text-white rounded-full flex items-center justify-center shadow-lg hover:bg-[#8b3a3a] hover:scale-110 transition-all duration-200 focus:outline-none focus:ring-4 focus:ring-pink-400"
+      className="fixed bottom-6 right-6 z-50 w-12 h-12  bg-[#c04242] text-white rounded-full flex items-center justify-center shadow-lg hover:bg-[#8b3a3a] hover:scale-110 transition-all duration-200 focus:outline-none focus:ring-4 focus:ring-pink-400"
       aria-label="Volver a regalos"
     >
       <ArrowLeft className="w-6 h-6" />
